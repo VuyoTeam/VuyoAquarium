a889c5fc127685db51601404c5e0bedbc55962c3
ti <ti@w10portatil20.IDEC-UPF.LOCAL>
2022-07-11 22:19:14 +0200

    Exemple pas de paràmetres amb tres tipus

--- VuyoAquariumProject/src/main/java/com/mycompany/vuyoaquariumproject/VuyoAquariumProject.java
+++ VuyoAquariumProject/src/main/java/com/mycompany/vuyoaquariumproject/VuyoAquariumProject.java
@@ -5,6 +5,7 @@
 
 package com.mycompany.vuyoaquariumproject;
 import VuyoBusiness.bsLectures;
+import Parametres.*;
 
 
 /**
@@ -15,7 +16,10 @@ import VuyoBusiness.bsLectures;
 public class VuyoAquariumProject {
 
     public static void main(String[] args) {
+        
+        /*
         int ph = 6, gh = 70, no3 = -5;
+        //Integer i = new Integer(-1);
         
         paramFuncioValids parametre = new paramFuncioValids();
               
@@ -27,8 +31,67 @@ public class VuyoAquariumProject {
         else {
             System.out.println(no3);
         }
-    }
         
+        */
+        
+        //*************** Primera  ****************************************************************************
+        int QuantClientsMajorsDe35i160cm = -1;
+        int edat = 35;
+        int altura = 160;
+        
+        QuantClientsMajorsDe35i160cm = QuantsClientsTinc(edat, altura);
+        
+        System.out.println(QuantClientsMajorsDe35i160cm);
+        
+        //**************  Segona *****************************************************************************
+        
+        paramsQuantsClientsTinc QuantsClients = new paramsQuantsClientsTinc();
+        
+        QuantsClientsTinc(35, altura, QuantsClients);
+        
+        System.out.println(QuantsClients.Quants);
+        System.out.println(QuantsClients.TempsLectura);
+        
+        //**************** Tercera   ***************************************************************************
+        paramsQuantsClientsTinc QuantsClients2Prova = new paramsQuantsClientsTinc();
+        
+        QuantsClients2Prova = QuantsClientsTincITempLectura(edat, altura);
+        
+        System.out.println(QuantsClients2Prova.Quants);
+        System.out.println(QuantsClients2Prova.TempsLectura);
+    }
+    
+    public static int QuantsClientsTinc(int pEdat, int pAltura)
+    {
+        //conectem a la BD, i mirem quants hi de la edat
+        int rQuants = -1;
+        
+        rQuants = 30;
+        
+        return rQuants;
+    }
+    
+    public static void QuantsClientsTinc(int pEdat, int pAltura, paramsQuantsClientsTinc pQuants)
+    {
+        //conectem a la BD, i mirem quants hi de la edat utilitzo parametres pedat i paltura
+        
+        pQuants.Quants = 30;
+        pQuants.TempsLectura = 1050;
+    }
+    
+    public static paramsQuantsClientsTinc QuantsClientsTincITempLectura(int pEdat, int pAltura)
+    {
+        //conectem a la BD, i mirem quants hi de la edat
+        paramsQuantsClientsTinc lQuantsTinc = new paramsQuantsClientsTinc();
+        
+        lQuantsTinc.Quants = 30;
+        lQuantsTinc.TempsLectura = 1050;
+        
+     
+        return lQuantsTinc;
+    }
+    
+    /*
     private static boolean ParametresValids(int pph, int pgh, int pno3 , paramFuncioValids pparametre)//, float  MitjaParams)
     {
         boolean valorAretornar = true;
@@ -46,6 +109,7 @@ public class VuyoAquariumProject {
         
         return valorAretornar;
     }
+    */
     
 }
 
