ff1ba939c9c2e3254f0892ca0d9f8d2286f52374
ti <ti@w10portatil20.IDEC-UPF.LOCAL>
2022-07-13 17:57:41 +0200

    exemple de menus

---
+++ VuyoAquariumProject/src/main/java/Parametres/ParametresMenu.java
@@ -0,0 +1,19 @@
+
+package Parametres;
+
+public class ParametresMenu {
+    
+    public int opcio1=-1;
+    public int opcio2=-1;
+    
+    
+    /**
+     * Aquest mètode serveix per resetejar els atributs d'aquesta classe
+     */
+    public void ResetejaOpcions()
+    {
+        opcio1=-1;
+        opcio2=-1;  
+    }
+       
+}
